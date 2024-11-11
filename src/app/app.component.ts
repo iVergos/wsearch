3c744fa0eb63d483f1c548331fd02400d367ff41
Panagiotis Vergos <panagos302@gmail.com>
2024-11-10 22:12:44 -0600

    Create: wsearch project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SearchBarComponent } from "./search-bar/search-bar.component";
+import { PageListComponent } from "./page-list/page-list.component";
+import { WikipediaService } from './wikipedia.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SearchBarComponent, PageListComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'wsearch';
+  pages: {}[] = [];
+
+  constructor(private wikipedia: WikipediaService) {
+
+  }
+
+  onTerm(term: string) {
+    this.wikipedia.search(term).subscribe(pages => {
+      this.pages = pages;
+    });
+  }
 }
